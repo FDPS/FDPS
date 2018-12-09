fda8a6e99bad266dcd92479d37d9edaee793e7a3
FDPS official <fdps_official@mail.jmlab.jp>
2018-12-10 00:36:58 +0900

    Inaccurate descriptions of the specification document of C++ are fixed.

--- src/ps_defs.hpp
+++ src/ps_defs.hpp
@@ -948,7 +948,7 @@ namespace ParticleSimulator{
             std::cerr << "     || ::      ::::::' ::      `......' ||"   << std::endl;
             std::cerr << "     ||     Framework for Developing     ||"   << std::endl;
             std::cerr << "     ||        Particle Simulator        ||"   << std::endl;
-            std::cerr << "     ||     Version 5.0a (2018/12)       ||"   << std::endl;
+            std::cerr << "     ||     Version 5.0b (2018/12)       ||"   << std::endl;
             std::cerr << "     \\\\==================================//" << std::endl;
             std::cerr << "" << std::endl;
             std::cerr << "       Home   : https://github.com/fdps/fdps " << std::endl;
